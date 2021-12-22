6c8900082b1a3c54e92c7743dd23077da3331858
huynen <vincent.huynen@gmail.com>
2021-12-22 14:22:25 +0100

    Update MailboxNotifierDecoder.js
    Adding uplink number info from decoder side rather than the rule's SQL Query from AWS side.

--- source/decoder/MailboxNotifierDecoder.js
+++ source/decoder/MailboxNotifierDecoder.js
@@ -1,4 +1,4 @@
-function Decoder(bytes, port) {
+function Decoder(bytes, port, uplink_info) {
 
 	//decode message type form payload
 	var data= bytes[0];
@@ -6,30 +6,30 @@ function Decoder(bytes, port) {
 	switch(data){
 		case 0x01: 
 			return {
-				msg: "You have got mail !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
+				msg: "Uplink n°"+ uplink_info.fcnt + "\nYou have got mail !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
 		};
 		case 0x02: 
 			return {
-				msg: "You have got maybe a parcel !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
+				msg: "Uplink n°"+ uplink_info.fcnt + "\nYou have got maybe a parcel !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
 		};
 		case 0x03: 
 			return {
-				msg: "[ERROR] Doors have been opened too long when you have turned on the MCU. Reboot the MCU to reinitialize it !",
+				msg: "\n[ERROR] Doors have been opened too long when you have turned on the MCU. Reboot the MCU to reinitialize it !",
 		};		
 		case 0x04: 
 			return {
-				msg: "[ERROR] Doors have been opened too long when you have fetched your mail. Reboot the MCU to re-initialize it !",
+				msg: "\n[ERROR] Doors have been opened too long when you have fetched your mail. Reboot the MCU to re-initialize it !",
 		};	
 		case 0x05: 
 			return {
-				msg: "[ERROR] Doors have been opened too long by the postman. Reboot the MCU to reinitialize it !",
+				msg: "\n[ERROR] Doors have been opened too long by the postman. Reboot the MCU to reinitialize it !",
 		};
 		case 0x06: 
 			return {
-				msg: "You have joined the people's network !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
+				msg: "Uplink n°"+ uplink_info.fcnt + "\nYou have joined the people's network !\nBattery Voltage: " + ((bytes[1]  << 8 ) + bytes[2])  /  1000  + " Volts\nBattery Level: " + ((((((bytes[1]  << 8 ) + bytes[2])  /  1000) -3.7)/(4.2-3.7))*100).toPrecision(4) +"%\nHave a nice day !",
 
 		};				
 		default:
-			return {msg:"unknown"};
+			return {msg:"\nunknown"};
 	}
 }
